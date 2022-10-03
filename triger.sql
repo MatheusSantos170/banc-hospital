0143c5b220bcf3cdb230c67810b82b87004e9428
Your Name <you@example.com>
2022-10-03 19:57:41 -0300

    take

--- triger.sql
+++ triger.sql
@@ -6,7 +6,17 @@ pac_row record;
 espec_row record;
 
 BEGIN
-    RAISE NOTICE 'nossa trigger rodou!!!';
+    SELECT INTO pac_row
+        *  FROM pacientes as p where p.id = NEW.pac_id;
+
+        SELECT INTO espec_row
+        *  FROM especialidades as esp where esp.id = NEW.especialidade_id;
+
+        IF pac_row.sexo = 'm' AND espec_row.nome = 'ginecologista' THEN
+           RAISE EXCEPTION 'Ginecologista apenas para pacientes do sexo feminino';
+        ELSEIF pac_row.sexo = 'f' AND espec_row.nome = 'urologista' THEN
+           RAISE EXCEPTION 'Urologista apenas para pacientes do sexo masculino';
+        END IF;
 RETURN new;
 
 END;
